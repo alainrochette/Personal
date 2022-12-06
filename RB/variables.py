cccd3c1e34b90e64ced7e97cacd5c09abbb7a985
alainrochette <alainrochette94@hotmail.com>
2022-12-06 00:39:45 -0500

    Update variables.py

--- RB/variables.py
+++ RB/variables.py
@@ -13,7 +13,8 @@ import requests
 dc = discogs_client.Client('ExampleApplication/0.1',user_token="AFgBxqbEvFlOWRwIcGtqrRGANUttwrfzncekMGbi")
 
 import pathlib
-fp = "/Users/alainrochette/MyGit/Personal/RB/"
+# fp = "/Users/alainrochette/MyGit/Personal/RB/"
+fp = ""
 
 def inKeys(keys, L):
 	if isinstance(keys, list):
